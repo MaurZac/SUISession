72f98876602ea73f338ba31411c1206a385a24f9
Maur Zac <maur.mx@gmail.com>
2022-09-29 15:42:08 -0500

    Initial Commit

---
+++ SUISession/SUISessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUISessionApp.swift
+//  SUISession
+//
+//  Created by C376855 on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SUISessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
